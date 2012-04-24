1dcfb55e96765f6613a2a821c3462c21ff737ecd
Mando Escamilla <mando@mando.org>
2012-04-24 14:50:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsBuildABackboneApp::Application.config.secret_token = '1860fbfe36524bb34b257f6dd68612acca0eb60c60111e436bb64c17d4e50512b2ffc8a020e33e04bad406b4fd1475387d15213fa6ad37f309fb2e81fe62b10c'
